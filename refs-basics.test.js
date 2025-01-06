5ef05c4a329740eba5370eaac51c6a87eca972bc
Jan Warchoł <jan.warchol@gmail.com>
2025-01-06 23:09:26 +0100

    Add example of actual reactivity

--- refs-basics.test.js
+++ refs-basics.test.js
@@ -1,7 +1,7 @@
 import { test, expect } from "vitest";
-import { ref } from "vue";
+import { ref, computed } from "vue";
 
-test("To access ref content, use .value property", () => {
+test("To get ref content, use .value property", () => {
   const aNumber = ref(1);
   expect(aNumber).toBeTypeOf("object");
   expect(aNumber.value).toBe(1);
@@ -20,8 +20,32 @@ test("To update ref content, assign to .value property", () => {
   expect(aNumber.value).toBe(5);
 });
 
-test.todo("Assigning to refs in component templates", () => {
+test.todo("Refs in component templates don't require using .value", () => {
   // Component templates allow using a shorthand notation:
   // const count = ref(0);
-  // <button @click="count++">Click count: {{ count }} </button>=
+  // <button @click="count++">Click count: {{ count }} </button>
+});
+
+test("Use computed to create chained reactive values", () => {
+  const aNumber = ref(1);
+  const doubled = computed(() => aNumber.value * 2);
+  expect(doubled.value).toBe(2);
+  aNumber.value = 3;
+  expect(doubled.value).toBe(6); // updates automagically
+});
+
+test("Simple expression assignment doesn't create a ref", async () => {
+  const aNumber = ref(1);
+  const doubled = aNumber.value * 2;
+  // Doubled is just a number, not a ref:
+  expect(doubled).toBe(2);
+  expect(doubled.value).toBeUndefined();
+});
+
+test("Simply creating a ref from expression is not reactive", () => {
+  const aNumber = ref(1);
+  const doubled = ref(aNumber.value * 2); // this won't be reactive
+  expect(doubled.value).toBe(2);
+  aNumber.value = 3;
+  expect(doubled.value).not.toBe(6);
 });
